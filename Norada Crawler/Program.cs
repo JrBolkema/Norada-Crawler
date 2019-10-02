6f32b84740a287791327dcb8af96138b8d82eb93
Jon Bolkema <jrbolkema@gmail.com>
2019-10-02 15:07:57 -0600

    idk

--- Norada Crawler/Program.cs
+++ Norada Crawler/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.Net.Http;
 
 namespace Norada_Crawler
 {
@@ -10,6 +12,25 @@ namespace Norada_Crawler
 	{
 		static void Main(string[] args)
 		{
+			StartCrawlerAsync();
+			Console.ReadLine();
+		}
+
+		private static async Task StartCrawlerAsync()
+		{
+			var url = "http://www.noradarealestate.com/Real-Estate-Investments/Illinois/Chicago/";
+			var httpClient = new HttpClient();
+			var html = await httpClient.GetStringAsync(url);
+			var htmlDocument = new HtmlDocument();
+			htmlDocument.LoadHtml(html);
+			//Console.WriteLine(htmlDocument.Text);
+			var contentDiv = htmlDocument.DocumentNode.Descendants("div")
+				.Where(node => node.GetAttributeValue("id", "").Equals("view1")).ToList();
+
+			foreach (var div in contentDiv)
+			{
+				Console.WriteLine(div.Descendants("table").ToString());
+			}
 		}
 	}
 }
