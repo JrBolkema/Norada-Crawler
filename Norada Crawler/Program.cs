f4382f5c4921ea70a4acd44aac7f07fc798ea3b3
Jon Bolkema <jrbolkema@gmail.com>
2019-10-03 12:26:13 -0600

    iidk lol

--- Norada Crawler/Program.cs
+++ Norada Crawler/Program.cs
@@ -27,9 +27,37 @@ namespace Norada_Crawler
 			var contentDiv = htmlDocument.DocumentNode.Descendants("div")
 				.Where(node => node.GetAttributeValue("id", "").Equals("view1")).ToList();
 
+
+			
 			foreach (var div in contentDiv)
 			{
-				Console.WriteLine(div.Descendants("table").ToString());
+				var tables = div.Descendants("table").ToList();
+				foreach (var table in tables)
+				{
+					var tableData = table.Descendants("td").ToList();
+
+					//foreach (var data in tableData)
+					//{
+					//	Console.WriteLine(data.InnerText.Trim().Length);
+					//	if (data.InnerText.Trim().Length > 75)
+					//	{
+					//		tableData.IndexOf(data);
+					//		tableData.RemoveAt(tableData.IndexOf(data));
+					//	}
+					//	//Console.WriteLine("DATA: " + data.InnerText.Trim());
+					//}
+
+					foreach (var data in tableData)
+					{
+						Console.WriteLine("DATA: " + data.InnerText.Trim());
+					}
+					//var tableData1 = table.Descendants("td")
+					//	.Where(node => node.GetAttributeValue("class", "").Equals("Prop-TD-1")).ToList();
+					//var tableData2 = table.Descendants("td")
+					//	.Where(node => node.GetAttributeValue("class", "").Equals("Prop-TD-2")).ToList();
+
+					
+				}
 			}
 		}
 	}
---
+++ Norada Crawler/PropertyListing.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Norada_Crawler
+{
+	public class PropertyListing
+	{
+		public string Location { get; set; }
+		public string PurchasePrice { get; set; }
+		public string RentalIncome { get; set; }
+		public string BedRooms { get; set; }
+		public string Bathrooms { get; set; }
+		public string Size { get; set; }
+		public string PricePerSqFoot { get; set; }
+		public string YearBuilt { get; set; }
+		public string Parking { get; set; }
+		public string Neighborhood { get; set; }
+		public string PropertyTax { get; set; }
+		public string InsuranceOrHOA { get; set; }
+		public string PropertyManagement { get; set; }
+		public string CashFlow { get; set; }
+		public string CapRate { get; set; }
+		public string RentValueRatio { get; set; }
+		public string DealGrader { get; set; }
+	}
+}
